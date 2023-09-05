70ab46e937ef5b12c335a7b56e742e1a6b461e6f
Bruno Riwerson Silva <riwerson@gmail.com>
2023-09-05 09:47:59 -0300

    feat: add Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+function Footer() {
+  return (
+    <footer>
+      <p>{'<dev> Bruno Riwerson Silva </dev>'}</p>
+
+      <a href="http://github.com/vanriwerson" target="_blank" rel="noopener noreferrer">Github</a>
+      <a href="http://www.linkedin.com/in/bruno-riwerson" target="_blank" rel="noopener noreferrer">Linkedin</a>
+      <a href="http://" target="_blank" rel="noopener noreferrer">Repo</a>
+    </footer>
+  )
+}
+
+export default Footer
