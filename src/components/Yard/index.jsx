aecbcd48b5ce99fbdd5d7e1cd54f20a24aa23d70
Bruno Riwerson Silva <riwerson@gmail.com>
2023-09-12 07:58:28 -0300

    feat: Yard structure

---
+++ src/components/Yard/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Yard() {
+  return (
+    <section className='yard'>
+      <div className="cats-container"></div>
+      <div className="boxes-container"></div>
+    </section>
+  )
+}
+
+export default Yard
